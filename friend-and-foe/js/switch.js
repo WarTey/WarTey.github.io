2969ce3a9c9c1a7a5581c87d09b8ef059d1dc1e3
Guillaume Blanc de Lanaute <guillaume.blanc-de-lanaute@isen.yncrea.fr>
2021-06-09 19:10:00 -0400

    Update Friend & Foe

--- friend-and-foe/js/switch.js
+++ friend-and-foe/js/switch.js
@@ -1,20 +1,36 @@
 var modal = document.getElementById("modal");
 var btnOpen = document.getElementById("open");
 var btnClose = document.getElementById("close");
+var loading = document.getElementById("loading");
 
-btnOpen.onclick = function() {
+function openModal() {
 	modal.style.display = "block";
 	btnOpen.style.display = "none";
 }
 
-btnClose.onclick = function () {
+function closeModal() {
 	modal.style.display = "none";
 	btnOpen.style.display = "block";
 }
 
-window.onclick = function(event) {
-	if (event.target == modal) {
-		modal.style.display = "none";
-		btnOpen.style.display = "block";
+btnOpen.onclick = function() { openModal(); }
+btnClose.onclick = function () { closeModal(); }
+
+window.onclick = function(event) { if (event.target == modal) closeModal(); }
+
+function fadeOut(el) {
+	el.style.opacity = 1;
+	(function fade() {
+		if ((el.style.opacity -= .1) < 0) el.style.display = "none";
+		else requestAnimationFrame(fade);
+	})();
+};
+
+document.onreadystatechange = function () {
+	if (document.readyState == 'complete') {
+		setTimeout(function () {
+			closeModal();
+			fadeOut(loading);
+		}, 2000);
 	}
 }
